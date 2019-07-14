d3bef9ad708cd4c1443b3744e552e4de121b1d3e
Pierson Brannan <pi@Piersons-MBP.fios-router.home>
2019-07-14 16:31:13 -0400

    Add Nav and Header component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,20 +1,31 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import About from "../About/About";
 import Audio from "../Audio/Audio";
+import Header from "../Header/Header";
 import Home from "../Home/Home";
+import Nav from "../Nav/Nav";
 import Products from "../Products/Products";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hi!</h1>
-        <button>Hello</button>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h2>Here be the header</h2>
+        </header>
+        <nav>
+          Here be the side nav bar
+        </nav>
+
+      </div>
+    );
+  }
 }
-
 export default App;
